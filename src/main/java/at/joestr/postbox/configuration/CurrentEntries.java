c33b16e412f7ebc8de26b3787dee4a5762798de3
Joel Strasser <strasser999@gmail.com>
2022-09-24 00:18:04 +0200

    puts sender name in item lore

--- src/main/java/at/joestr/postbox/configuration/CurrentEntries.java
+++ src/main/java/at/joestr/postbox/configuration/CurrentEntries.java
@@ -33,54 +33,57 @@ import java.util.Optional;
  */
 public enum CurrentEntries {
   // Configuration entries
-  CONF_VERSION("version", new int[] {1}),
-  CONF_JDBCURI("jdbcUri", new int[] {1}),
-  CONF_SIZE("size", new int[] {1}),
-  CONF_UPDATER_ENABLED("updater.enabled", new int[] {1}),
-  CONF_UPDATER_DOWNLOADTOPLUGINUPDATEFOLDER("updater.downloadToPluginUpdateFolder", new int[] {1}),
-  CONF_UPDATER_TARGETURL("updater.targetUrl", new int[] {1}),
-  CONF_UPDATER_POMPROPERTIESFILE("updater.pomPropertiesFile", new int[] {1}),
-  CONF_UPDATER_CLASSIFIER("updater.classifier", new int[] {1}),
+  CONF_VERSION("version"),
+  CONF_JDBCURI("jdbcUri"),
+  CONF_SIZE("size"),
+  CONF_UPDATER_ENABLED("updater.enabled"),
+  CONF_UPDATER_DOWNLOADTOPLUGINUPDATEFOLDER("updater.downloadToPluginUpdateFolder"),
+  CONF_UPDATER_TARGETURL("updater.targetUrl"),
+  CONF_UPDATER_POMPROPERTIESFILE("updater.pomPropertiesFile"),
+  CONF_UPDATER_CLASSIFIER("updater.classifier"),
   // Languages
-  LANG_VERSION("version", new int[] {1}),
-  LANG_PREFIX("prefix", new int[] {1}),
-  LANG_GEN_NOT_A_PLAYER("generic.not_a_player", new int[] {1}),
-  LANG_CMD_POSTBOX_X_MSG_SEND("commands.postbox.message_send", new int[] {1}),
-  LANG_CMD_POSTBOX_X_MSG_OPEN("commands.postbox.message_open", new int[] {1}),
-  LANG_CMD_POSTBOX_X_MSG_OPENOTHER("commands.postbox.message_openother", new int[] {1}),
-  LANG_CMD_POSTBOX_X_MSG_UPDATE("commands.postbox.message_update", new int[] {1}),
-  LANG_CMD_POSTBOX_SEND_SELF("commands.postbox-send.self", new int[] {1}),
+  LANG_VERSION("version"),
+  LANG_PREFIX("prefix"),
+  LANG_GEN_NOT_A_PLAYER("generic.not_a_player"),
+  LANG_CMD_POSTBOX_X_MSG_SEND("commands.postbox.message_send"),
+  LANG_CMD_POSTBOX_X_MSG_OPEN("commands.postbox.message_open"),
+  LANG_CMD_POSTBOX_X_MSG_OPENOTHER("commands.postbox.message_openother"),
+  LANG_CMD_POSTBOX_X_MSG_UPDATE("commands.postbox.message_update"),
+  LANG_CMD_POSTBOX_SEND_SELF("commands.postbox-send.self"),
   LANG_CMD_POSTBOX_SEND_RECEIVER_NEVER_PLAYED(
-      "commands.postbox-send.receiver_never_played", new int[] {1}),
-  LANG_CMD_POSTBOX_SEND_SEND_EMPTY("commands.postbox-send.send_empty", new int[] {1}),
-  LANG_CMD_POSTBOX_SEND_SUCCESS_RECEIVER("commands.postbox-send.success_receive", new int[] {1}),
-  LANG_CMD_POSTBOX_SEND_SUCCESS_SENDER("commands.postbox-send.success_sender", new int[] {1}),
-  LANG_CMD_POSTBOX_SEND_RECEIPIENT_FULL("commands.postbox-send.receipient_full", new int[] {1}),
-  LANG_CMD_POSTBOX_OPEN_CHEST_TITLE("commands.postbox-open.chest_title", new int[] {1}),
-  LANG_CMD_POSTBOX_OPEN_EMPTY("commands.postbox-open.empty", new int[] {1}),
-  LANG_CMD_POSTBOX_OPENOTHER_CHEST_TITLE("commands.postbox-openother.chest_title", new int[] {1}),
-  LANG_CMD_POSTBOX_OPENOTHER_EMPTY("commands.postbox-openother.empty", new int[] {1}),
-  LANG_CMD_POSTBOX_UPDATE_OFF("commands.postbox-update.off", new int[] {1}),
-  LANG_CMD_POSTBOX_UPDATE_ASYNCSTART("commands.postbox-update.asyncstart", new int[] {1}),
-  LANG_CMD_POSTBOX_UPDATE_ERROR("commands.postbox-update.error", new int[] {1}),
-  LANG_CMD_POSTBOX_UPDATE_UPTODATE("commands.postbox-update.uptodate", new int[] {1}),
-  LANG_CMD_POSTBOX_UPDATE_AVAILABLE("commands.postbox-update.available", new int[] {1}),
-  LANG_CMD_POSTBOX_UPDATE_DOWNLOADED("command.postbox-update.downloaded", new int[] {1}),
-  LANG_EVT_MESSAGE_ON_JOIN("events.message_on_join", new int[] {1}),
-  LANG_EVT_INVENTORY_FULL("events.inventory_full", new int[] {1}),
+    "commands.postbox-send.receiver_never_played"),
+  LANG_CMD_POSTBOX_SEND_SEND_EMPTY("commands.postbox-send.send_empty"),
+  LANG_CMD_POSTBOX_SEND_SUCCESS_RECEIVER("commands.postbox-send.success_receive"),
+  LANG_CMD_POSTBOX_SEND_SUCCESS_SENDER("commands.postbox-send.success_sender"),
+  LANG_CMD_POSTBOX_SEND_RECEIPIENT_FULL("commands.postbox-send.receipient_full"),
+  LANG_CMD_POSTBOX_OPEN_CHEST_TITLE("commands.postbox-open.chest_title"),
+  LANG_CMD_POSTBOX_OPEN_EMPTY("commands.postbox-open.empty"),
+  LANG_CMD_POSTBOX_OPEN_PLAYERNAME_RESOLVING_ERROR(
+    "commands.postbox-open.playername_resolving_error"),
+  LANG_CMD_POSTBOX_OPENOTHER_CHEST_TITLE(
+    "commands.postbox-openother.chest_title"),
+  LANG_CMD_POSTBOX_OPENOTHER_EMPTY("commands.postbox-openother.empty"),
+  LANG_CMD_POSTBOX_OPENOTHER_PLAYERNAME_RESOLVING_ERROR(
+    "commands.postbox-openother.playername_resolving_error"),
+  LANG_CMD_POSTBOX_UPDATE_OFF("commands.postbox-update.off"),
+  LANG_CMD_POSTBOX_UPDATE_ASYNCSTART("commands.postbox-update.asyncstart"),
+  LANG_CMD_POSTBOX_UPDATE_ERROR("commands.postbox-update.error"),
+  LANG_CMD_POSTBOX_UPDATE_UPTODATE("commands.postbox-update.uptodate"),
+  LANG_CMD_POSTBOX_UPDATE_AVAILABLE("commands.postbox-update.available"),
+  LANG_CMD_POSTBOX_UPDATE_DOWNLOADED("command.postbox-update.downloaded"),
+  LANG_EVT_MESSAGE_ON_JOIN("events.message_on_join"),
+  LANG_EVT_INVENTORY_FULL("events.inventory_full"),
   // Permissions
-  PERM_CMD_POSTBOX("postbox.commands.postbox", new int[] {0}),
-  PERM_CMD_POSTBOX_SEND("postbox.commands.postbox-send", new int[] {0}),
-  PERM_CMD_POSTBOX_OPEN("postbox.commands.postbox-open", new int[] {0}),
-  PERM_CMD_POSTBOX_OPENOTHER("postbox.commands.postbox-openother", new int[] {0}),
-  PERM_CMD_POSTBOX_UPDATE("postbox.commands.postbox-update", new int[] {0});
+  PERM_CMD_POSTBOX("postbox.commands.postbox"),
+  PERM_CMD_POSTBOX_SEND("postbox.commands.postbox-send"),
+  PERM_CMD_POSTBOX_OPEN("postbox.commands.postbox-open"),
+  PERM_CMD_POSTBOX_OPENOTHER("postbox.commands.postbox-openother"),
+  PERM_CMD_POSTBOX_UPDATE("postbox.commands.postbox-update");
 
   private final String text;
-  private final int[] inVersions;
 
-  private CurrentEntries(String text, int[] inVersions) {
+  private CurrentEntries(String text) {
     this.text = text;
-    this.inVersions = inVersions;
   }
 
   @Override
@@ -89,10 +92,10 @@ public enum CurrentEntries {
   }
 
   public static CurrentEntries find(String text) {
-    Optional<CurrentEntries> result =
-        Arrays.asList(values()).stream()
-            .filter(cE -> cE.toString().equalsIgnoreCase(text))
-            .findFirst();
+    Optional<CurrentEntries> result
+      = Arrays.asList(values()).stream()
+        .filter(cE -> cE.toString().equalsIgnoreCase(text))
+        .findFirst();
     if (result.isPresent()) {
       return result.get();
     }
@@ -100,7 +103,7 @@ public enum CurrentEntries {
   }
 
   public static CurrentEntries[] getConfigurationEntries() {
-    return new CurrentEntries[] {
+    return new CurrentEntries[]{
       CONF_VERSION,
       CONF_JDBCURI,
       CONF_SIZE,
@@ -113,7 +116,7 @@ public enum CurrentEntries {
   }
 
   public static CurrentEntries[] getLanguageEntries() {
-    return new CurrentEntries[] {
+    return new CurrentEntries[]{
       LANG_VERSION,
       LANG_PREFIX,
       LANG_GEN_NOT_A_PLAYER,
@@ -129,8 +132,10 @@ public enum CurrentEntries {
       LANG_CMD_POSTBOX_SEND_RECEIPIENT_FULL,
       LANG_CMD_POSTBOX_OPEN_CHEST_TITLE,
       LANG_CMD_POSTBOX_OPEN_EMPTY,
+      LANG_CMD_POSTBOX_OPEN_PLAYERNAME_RESOLVING_ERROR,
       LANG_CMD_POSTBOX_OPENOTHER_CHEST_TITLE,
       LANG_CMD_POSTBOX_OPENOTHER_EMPTY,
+      LANG_CMD_POSTBOX_OPENOTHER_PLAYERNAME_RESOLVING_ERROR,
       LANG_CMD_POSTBOX_UPDATE_OFF,
       LANG_CMD_POSTBOX_UPDATE_ASYNCSTART,
       LANG_CMD_POSTBOX_UPDATE_ERROR,
@@ -143,7 +148,7 @@ public enum CurrentEntries {
   }
 
   public static CurrentEntries[] getPermissionEntries() {
-    return new CurrentEntries[] {
+    return new CurrentEntries[]{
       PERM_CMD_POSTBOX,
       PERM_CMD_POSTBOX_SEND,
       PERM_CMD_POSTBOX_OPEN,
