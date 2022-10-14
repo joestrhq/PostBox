a004efbde90fd1f09bcc9e09203c58ad37889990
Joel Strasser <strasser999@gmail.com>
2022-10-14 21:25:59 +0200

    implements update command

--- src/main/java/at/joestr/postbox/commands/CommandPostBoxUpdate.java
+++ src/main/java/at/joestr/postbox/commands/CommandPostBoxUpdate.java
@@ -24,31 +24,102 @@
 package at.joestr.postbox.commands;
 
 import at.joestr.postbox.PostBoxPlugin;
+import at.joestr.postbox.configuration.CurrentEntries;
+import at.joestr.postbox.configuration.LocaleHelper;
+import at.joestr.postbox.configuration.MessageHelper;
+import at.joestr.postbox.configuration.Updater.State;
+import java.util.Collections;
 import java.util.List;
+import java.util.Locale;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabExecutor;
+import org.bukkit.entity.Player;
 
 /**
  * @author joestr
  */
 public class CommandPostBoxUpdate implements TabExecutor {
 
-  public CommandPostBoxUpdate(PostBoxPlugin aThis) {
-  }
-
   @Override
   public List<String> onTabComplete(
-    CommandSender cs, Command cmnd, String string, String[] strings) {
-    throw new UnsupportedOperationException(
-      "Not supported yet."); // Generated from
-    // nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    CommandSender sender, Command command, String alias, String[] args) {
+    return Collections.emptyList();
   }
 
   @Override
-  public boolean onCommand(CommandSender cs, Command cmnd, String string, String[] strings) {
-    throw new UnsupportedOperationException(
-      "Not supported yet."); // Generated from
-    // nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+  public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+    if (args.length != 0) {
+      return false;
+    }
+
+    final Locale locale
+      = sender instanceof Player
+        ? LocaleHelper.resolve(((Player) sender).getLocale())
+        : Locale.ENGLISH;
+
+    if (!(sender instanceof Player)) {
+      new MessageHelper()
+        .prefix(true)
+        .path(CurrentEntries.LANG_GEN_NOT_A_PLAYER)
+        .locale(locale)
+        .receiver(sender)
+        .send();
+      return true;
+    }
+
+    new MessageHelper()
+      .prefix(true)
+      .path(CurrentEntries.LANG_CMD_POSTBOX_UPDATE_ASYNCSTART)
+      .locale(locale)
+      .receiver(sender)
+      .send();
+
+    PostBoxPlugin.getInstance()
+      .getUpdater()
+      .checkForUpdate()
+      .whenCompleteAsync(
+        (optionalUpdate, error) -> {
+          if (error != null) {
+            new MessageHelper()
+              .prefix(true)
+              .path(CurrentEntries.LANG_CMD_POSTBOX_UPDATE_ERROR)
+              .locale(locale)
+              .receiver(sender)
+              .send();
+            return;
+          }
+
+          if (optionalUpdate.equals(State.SUCCESS_UPTODATE)) {
+            new MessageHelper()
+              .prefix(true)
+              .path(CurrentEntries.LANG_CMD_POSTBOX_UPDATE_UPTODATE)
+              .locale(locale)
+              .receiver(sender)
+              .send();
+            return;
+          }
+
+          if (optionalUpdate.equals(State.SUCCESS_AVAILABLE)) {
+            new MessageHelper()
+              .prefix(true)
+              .path(CurrentEntries.LANG_CMD_POSTBOX_UPDATE_AVAILABLE)
+              .locale(locale)
+              .receiver(sender)
+              .modify(msg -> msg.replace("%update$downloadUrl", "...."))
+              .send();
+            return;
+          }
+
+          if (optionalUpdate.equals(State.SUCCES_DOWNLOADED)) {
+            new MessageHelper()
+              .prefix(true)
+              .path(CurrentEntries.LANG_CMD_POSTBOX_UPDATE_DOWNLOADED)
+              .locale(locale)
+              .receiver(sender)
+              .send();
+          }
+        });
+
+    return true;
   }
-}
